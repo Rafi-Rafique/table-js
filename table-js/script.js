380443613f4aa4b3ddd69c86142fa7cfe4443794
Rafi-Rafique <160730669+Rafi-Rafique@users.noreply.github.com>
2024-05-20 01:51:52 +0500

    Add files via upload
    I wanted to share a quick and simple tutorial on how to create a table using JavaScript. Whether you're a seasoned developer or just starting out, mastering the basics can significantly enhance your skillset.

---
+++ table-js/script.js
@@ -0,0 +1,39 @@
+
+// Table create from javascript
+
+var tabName = prompt("Enter table no");
+
+var one = (1);
+var two = (2);
+var three = (3);
+var four = (4);
+var five = (5);
+var six = (6);
+var seven = (7);
+var eight = (8);
+var nine = (9);
+var ten = (10);
+
+var mulTab1 = (tabName * one);
+var mulTab2 = (tabName * two);
+var mulTab3 = (tabName * three);
+var mulTab4 = (tabName * four);
+var mulTab5 = (tabName * five);
+var mulTab6 = (tabName * six);
+var mulTab7 = (tabName * seven);
+var mulTab8 = (tabName * eight);
+var mulTab9 = (tabName * nine);
+var mulTab10 = (tabName * ten);
+
+
+document.write("<h1>" + "<i>" + "<center>" + "Table of " + tabName + "</center>" + "<i>" + "</h1>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + one + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab1 + "<td>" + "</tr>" + "</table>" + "</center>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + two + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab2 + "<td>" + "</tr>" + "</table>" + "</center>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + three + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab3 + "<td>" + "</tr>" + "</table>" + "</center>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + four + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab4 + "<td>" + "</tr>" + "</table>" + "</center>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + five + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab5 + "<td>" + "</tr>" + "</table>" + "</center>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + six + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab6 + "<td>" + "</tr>" + "</table>" + "</center>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + seven + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab7 + "<td>" + "</tr>" + "</table>" + "</center>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + eight + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab8 + "<td>" + "</tr>" + "</table>" + "</center>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + nine + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab9 + "<td>" + "</tr>" + "</table>" + "</center>");
+document.write("<center>" + "<table'border-1'>" + "<tr>" + "<td>" + tabName + "<td>" + "<td>" + " X " + "<td>" + "<td>" + ten + "<td>" + "<td>" + " = " + "<td>" + "<td>" + mulTab10 + "<td>" + "</tr>" + "</table>" + "</center>");
